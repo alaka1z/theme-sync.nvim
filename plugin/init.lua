822a74ae21d7208b886564813a3e715614da74de
alaka <alakazoidiac@gmail.com>
2026-09-07 20:26:15 +1200

    Restore saved WezTerm theme on startup

--- plugin/init.lua
+++ plugin/init.lua
@@ -6,6 +6,33 @@ local wezterm = require("wezterm")
 
 local M = {}
 
+local themes = {
+  catppuccin = "Catppuccin Mocha",
+  tokyonight = "Tokyo Night Moon",
+  gruvbox = "GruvboxDark",
+  rose_pine = "rose-pine",
+}
+
+local function load_saved_theme()
+  local local_appdata = os.getenv("LOCALAPPDATA")
+
+  if not local_appdata then
+    return themes.catppuccin
+  end
+
+  local path = local_appdata .. "\\theme-sync\\theme"
+  local file = io.open(path, "r")
+
+  if not file then
+    return themes.catppuccin
+  end
+
+  local id = file:read("*l")
+  file:close()
+
+  return themes[id] or themes.catppuccin
+end
+
 local function apply_dynamic_scheme(pane, scheme_name)
   local schemes = wezterm.color.get_builtin_schemes()
   local scheme = schemes[scheme_name]
@@ -49,6 +76,8 @@ local function apply_dynamic_scheme(pane, scheme_name)
 end
 
 function M.apply_to_config(config)
+  config.color_scheme = load_saved_theme()
+
   wezterm.on("user-var-changed", function(window, pane, name, value)
     if name ~= "THEME_SYNC" then
       return
